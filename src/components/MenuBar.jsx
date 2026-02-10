129a3cffb1c58f6a3a2731ba32810da5ce213e37
ember <sixemod@protonmail.com>
2026-02-10 19:08:29 +0100

    push / started styling, currently still still debugging, asked for help on #code

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -6,26 +6,28 @@ import HomeIcon from "../assets/icons/HomeIcon.jsx";
 
 function MenuBar() {
     const navigate = useNavigate();
-
+//w-full h-32px flex justify-center-safe space
     return (
-        <div className="w-full h-32px flex justify-center-safe bg-[#d4d2d2] dark:bg-[#111111] space">
-            <div className="flex items-center">
-                <div className="HOME-BUTTON"
+        <div className="flex h-32 w-full bg-[#d4d2d2] dark:bg-[#111111] ">
+
+            <div className="justify-center h-32 static right-0"
                      onClick={() => navigate("/")}
-                >
-                    <HomeIcon/>
-                </div>
-                <span className="ml-1">Ember</span>
+            >
+                <HomeIcon/>
             </div>
 
-            <div className="flex items-center space-4">
-                <div className="flex items-center mx"
+            <div className="justify-center">
+                <span className="">Ember</span>
+            </div>
+
+            <div className="flex space-x-4">
+                <div className="flex justify-center space-x-4"
                      onClick={() => navigate("/contact")}
                 >
                     <span className="ml-1">Contact</span>
                     <ContactIcon/>
                 </div>
-                <div className="flex items-center"
+                <div className="flex justify-center space-x-4"
                      onClick={() => navigate("/about")}
                 >
                     <span className="ml-1">About me</span>
